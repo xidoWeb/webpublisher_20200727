75ff0c91bcdb192d9c1859ebd585170579684421
xidoweb <xidoweb@gmail.com>
2020-10-21 17:45:32 +0900

    rwd base

--- c_code/d_javascript/js/loop_02.js
+++ c_code/d_javascript/js/loop_02.js
@@ -26,16 +26,81 @@
 //   console.log(k);
 // }
 // console.log('k: ' , k);
+/*
+  var arr = ['포도', '사과', '바나나','딸기'];
+  var classN = ['grape','apple','banana','straw'];
+  var testEl = document.getElementsByClassName('test')[0];
+  // 요소를 삽입하기조건: 1.요소생성, 2.요소내용기입, 3.요소삽입
+  var pEl;
 
-var arr = ['포도', '사과', '바나나','딸기'];
-var testEl = document.getElementsByClassName('test')[0];
-// 요소를 삽입하기조건: 1.요소생성, 2.요소내용기입, 3.요소삽입
-var pEl;
+  var i = 0;
+  for(; i < 4; i += 1){
+    pEl = document.createElement('p');
+    pEl.innerText = arr[i] + '를 먹는다';
+
+    // 각각의 p에 class이름을 삽입해보세요
+    // 1. setAttribute : 속성을변경이가능(어떠한 속성명이든 관계없다.) ===========
+    // pEl.setAttribute('title', classN[i]);
+    // 2. className : 속성중 오직 class속성만 이름변경 ===========================
+    // pEl.className = classN[i];
+    // 3. classList : 클래스 속성을 리스트처럼 여러개 설정 가능 ==================
+    pEl.classList = classN[i] ;
+    pEl.classList.add('testClass');
+    // pEl.classList.add( classN[i] );  
+    // pEl.classList.remove( classN[i] );
+    // pEl.classList.toggle( classN[i] );
+    testEl.append(pEl);
+    // console.log(arr[i]);
+  }
+*/
+
+
+// for(최초의값; 조건; 연산수행){}
+
+var coffee = ['esspress','americano','latte','flat white','cafe mocha', 'long black'];
+// for(var j=0; j< coffee.length ; j+=1){
+//   console.log('1:', coffee[j] );
+// }
+
+// for ~ in 문법은 객체를 위한 형식, 배열을 위해만든 형식이 아니다.
+// for(var i in coffee){  console.log('2:', coffee[i]); }
+
+// 배열형식을 사용할때에는 for, forEach를 사용
+// coffee.forEach(function(data, index){
+//   console.log( data, index );
+//  });
+
+// for ~ in 을 위해서는 객체를 위해 사용
+var obj = {
+  'spring': 'black tea',
+  'summer': 'ice americano',
+  'fall'  : 'hot choch',
+  'winter': 'pepsi'
+};
+// var o = 0;
+// for(; o < 4; o += 1){  console.log( obj.spring ); }
+// for(var o in obj){
+//   // obj.spring
+//   // obj.summer
+//   // obj.fall
+//   // obj.winter
+//   // console.log( o ); 
+//   console.log( obj[o] );
+// }
+
+// 반복문
+// for, forEach, for-in
 
 var i = 0;
-for(; i < 4; i += 1){
-  pEl = document.createElement('p');
-  pEl.innerText = arr[i] + '를 먹는다';
-  testEl.append(pEl);
-  // console.log(arr[i]);
+for(; i<100; i+=1 ){
+
+  if(i < 10){
+    console.log( '0' + i );
+  }else if(i === 50){
+    // break;
+    continue;
+  }else{
+    console.log( i );
+  }
+  
 }
