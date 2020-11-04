676b0dafce2f6c9214ebf0873f609d33f8f24a9d
xidoweb <xidoweb@gmail.com>
2020-11-04 10:35:18 +0900

    jquery base 001

---
+++ c_code/e_jquery/js/src/jq_01_selector.js
@@ -0,0 +1,13 @@
+// jq_01_selector.js
+// $(document).ready(function(){});
+// $(function(){});
+
+
+(function($){
+  var idBox = $('#box');
+  idBox.animate({
+    'width':100 + 'px', 
+    'height':100 + 'px', 
+    'backgroundColor':'#fa0'});
+
+})(jQuery);
