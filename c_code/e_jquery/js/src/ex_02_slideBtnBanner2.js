4d2189cabbe7ce8039eb93601df80160563e8ede
xido <xidoweb@gmail.com>
2020-11-11 14:14:19 +0900

    fade slide정리

--- c_code/e_jquery/js/src/ex_02_slideBtnBanner2.js
+++ c_code/e_jquery/js/src/ex_02_slideBtnBanner2.js
@@ -4,20 +4,14 @@
   var slide = $('.slide_02');
   var slideBtnPart = slide.find('.slide_btn');
   var slideBtn = slideBtnPart.children('button');
-
   var indexSlide = slide.find('.index_slide');
   var slideLi = indexSlide.find('li');
   var liLen = slideLi.length;
-
   var indexN = 0;
   var permission = true;
   var timed = 500;
-
+  
   slideLi.not( $('.action') ).hide();
-  /*
-  slideLi.hide();
-  indexSlide.find('.action').show();
-  */
 // action클래스이름이 몇번째 있는지 파악하는 기능;
  var actionIndex;
  var MyActionIndex = function(){
@@ -37,56 +31,32 @@
   slideLi.eq(actionIndex).stop().fadeOut(timed/2, function(){
     slideLi.eq(indexN).siblings().removeClass('action');
     slideLi.eq(indexN).addClass('action');
-
     setTimeout(function(){
       permission = true;
     }, timed/5);
-
   }); // fadeOut()
  }; // SlideAction()
-
  // -------------------------------------------------
-
-
  slideBtn.on('click', function(e){
   e.preventDefault();
   MyActionIndex();
-
   if(permission){
     permission = false; 
     var it = $(this).attr('class');
-
     if(it === 'next'){
       indexN += 1;
       if(indexN >= liLen ){
         indexN = 0;
       } // if()
-
-      // slideLi.eq(indexN).show();
-      // slideLi.eq(actionIndex).fadeOut(function(){
-      //   slideLi.eq(indexN).siblings().removeClass('action');
-      //   slideLi.eq(indexN).addClass('action');
-      // }); // fadeOut()
       SlideAction();
-
     }else if( it === 'prev'){
-      indexN -= 1;
-      // slideLi.eq(indexN).show();
-      //   slideLi.eq(actionIndex).fadeOut(function(){
-      //     slideLi.eq(indexN).siblings().removeClass('action');
-      //     slideLi.eq(indexN).addClass('action');
-      // }); // fadeOut()
+      indexN -= 1;     
       SlideAction();
-
       if(indexN <= -1){
         indexN = liLen -1;      
       }// if()
-
     }// if( it === 'prev') End ------------------
-    
-    console.log( indexN );
   } // if(permission)
-
  }); // slideBtn.on('click') End
 
 })(jQuery);
