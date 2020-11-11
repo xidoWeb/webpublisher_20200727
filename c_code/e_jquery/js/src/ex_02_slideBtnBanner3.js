fea6ab18339e8e2b9f35c58572e7e212cb3a1564
xido <xidoweb@gmail.com>
2020-11-11 16:42:21 +0900

    focus처리 및 tabindex 처리

--- c_code/e_jquery/js/src/ex_02_slideBtnBanner3.js
+++ c_code/e_jquery/js/src/ex_02_slideBtnBanner3.js
@@ -7,18 +7,27 @@
 
   var slideWrap = indiSlide.find('.slide_wrap');
   var slideUl = slideWrap.children('ul');
+  var slideLi  = slideUl.children('li');
+  var slideLiLink = slideLi.children('a');
 
   var permission = true;
   var indiSlN = 0;
   var timed = 500; 
 
-  // indicator클릭시 ul이동
-  indiLi.on('focus', ['a'], function(e){
+  // slide_wrap 내부 a focus기능 강제 비처리
+  slideLiLink.attr({'tabIndex':'-1'});
+
+
+  // indicator클릭시 ul이동 -> a에 focus처리로 변경하고, 실제 배너에 a는 별도로 focus처리
+  // 클릭기능 일단 보류
+  indiLi.on('click', function(e){ e.preventDefault(); });
+
+  indiLi.children('a').on('focus', function(e){
     e.preventDefault();
     if(permission){
       permission = false; 
       var it = $(this);
-      indiSlN = it.index();
+      indiSlN = it.parent('li').index();
       slideUl.stop().animate({'marginLeft': indiSlN * -100 + '%'}, function(){
         indiLi.eq(indiSlN).addClass('action');
         indiLi.eq(indiSlN).siblings().removeClass('action');
