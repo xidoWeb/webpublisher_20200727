996536f92bf771e32b0db1e25313c04d8419e789
xido <xidoweb@gmail.com>
2020-11-11 17:45:39 +0900

    focus and click 중복 처리 해결전 테스트

--- c_code/e_jquery/js/src/ex_02_slideBtnBanner3.js
+++ c_code/e_jquery/js/src/ex_02_slideBtnBanner3.js
@@ -17,27 +17,51 @@
   // slide_wrap 내부 a focus기능 강제 비처리
   slideLiLink.attr({'tabIndex':'-1'});
 
+  var typeTest = function(evt){
+    // console.log( evt.type );
+    if(permission && evt.type === 'focus'){
+      permission = false;
+      console.log('포커스 처리되었음');
+    }else if(permission && evt.type === 'click'){
+      permission = false;
+      console.log('클릭 되었음');
+    }else {
+      console.log('...');
+    }   
+  };
 
   // indicator클릭시 ul이동 -> a에 focus처리로 변경하고, 실제 배너에 a는 별도로 focus처리
-  // 클릭기능 일단 보류
-  indiLi.on('click', function(e){ e.preventDefault(); });
-
   indiLi.children('a').on('focus', function(e){
     e.preventDefault();
-    if(permission){
-      permission = false; 
-      var it = $(this);
-      indiSlN = it.parent('li').index();
-      slideUl.stop().animate({'marginLeft': indiSlN * -100 + '%'}, function(){
-        indiLi.eq(indiSlN).addClass('action');
-        indiLi.eq(indiSlN).siblings().removeClass('action');
 
-        setTimeout(function(){
-          permission = true;
-        }, timed/5);
-      });
-    } // if(permission)
-  }); // indiLi.on('click')
+    typeTest(e);
 
+    var it = $(this);
+    indiSlN = it.parent('li').index();      
+    // console.log(indiSlN);
+    indiLi.eq(indiSlN).addClass('action');
+    indiLi.eq(indiSlN).siblings().removeClass('action');       
+    slideUl.stop().css({'marginLeft': indiSlN * -100 + '%'});
+   
+  }); // indiLi.children('a).on('focus')
+
+  // 클릭기능 수행
+  indiLi.on('click', function(e){ 
+    e.preventDefault(); 
+
+    typeTest(e);
+
+    var it = $(this);
+    indiSlN = it.index();
+    var propertyLink = it.children('a').attr('href');
+    var thatPosition = $(propertyLink);
+
+    // slideUl.stop().css({'marginLeft': indiSlN * -100 + '%'});
+    // indiLi.eq(indiSlN).addClass('action');
+    // indiLi.eq(indiSlN).siblings().removeClass('action');       
+    // slideUl.stop().css({'marginLeft': indiSlN * -100 + '%'});
+    thatPosition.attr({'tabIndex':'0'});
+    thatPosition.focus();
+  });
 
 })(jQuery);
