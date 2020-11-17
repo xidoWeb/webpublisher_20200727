7e8bb19c571dd6a726b6905046c294de8c252229
xido <xidoweb@gmail.com>
2020-11-17 14:55:03 +0900

    create gallery_box, remove gallery_box

--- c_code/e_jquery/js/src/ex_03_modal.js
+++ c_code/e_jquery/js/src/ex_03_modal.js
@@ -33,8 +33,7 @@
   var galleryCode = '<div class="gallery_box"><div class="gallery_area"><div class="close"><button type="button"><span>닫기</span></button></div><div class="big_img"><p></p></div></div></div>';
   // -------------------------------------------------------------
   var modal01 = $('.modal_01');  
-  // modal01.append(galleryCode);   // 선택자 내부의 뒤에
-  modal01.after(galleryCode);       // 선택자 바로 뒤에
+  
   
   var modalUl = modal01.children('ul');
   var i=0;
@@ -44,8 +43,7 @@
   for(; i<data.length; i+=1){
     modalUl.append(modaInCode);
     temLi = modalUl.children('li').eq(-1);
-    // temLi.attr({'data-big':data[i].bigImg, 'data-narr':data[i].content});
-    
+       
     modalBtn = temLi.find('button');
     modalBtn.css({
       backgroundImage:'url('+ url + data[i].thumImg +')', 
@@ -57,23 +55,22 @@
   var modalLi = modal01.find('li');
   var mLiBtn = modalLi.find('button');
 
-  var gallery = $('.gallery_box');
-  var galleryClose = gallery.find('.close');
-  var bigImg = gallery.find('.big_img');
-  var bigNarr = bigImg.children('p');
-
-  var timed = 500;
+  var gallery, galleryClose, bigImg, bigNarr;
   var indexChdek;
+  var timed = 500;
+
   mLiBtn.on('click', function(e){
     e.preventDefault();
     var it = $(this).parent('li');
     var itI = it.index();
     indexChdek = itI;
-    // var thisBig = it.attr('data-big');
-    // var thisNarr = it.attr('data-narr');
-    // bigImg.css({backgroundImage: 'url(' + url + thisBig +')'});
-    // bigNarr.text( thisNarr );
-    
+  //  -------------------------------
+    modal01.after(galleryCode);       // 선택자 바로 뒤에
+    gallery = $('.gallery_box');
+    galleryClose = gallery.find('.close');
+    bigImg = gallery.find('.big_img');
+    bigNarr = bigImg.children('p');
+  //  -------------------------------
     bigImg.css({backgroundImage: 'url(' + url + data[indexChdek].bigImg +')'});
     bigNarr.text( data[indexChdek].content );
 
@@ -83,13 +80,15 @@
         bigNarr.addClass('action');
       }, timed/2)
     });
-  });
-
+//  -------------------------------
   galleryClose.on('click', function(e){
     e.preventDefault();
     gallery.stop().fadeOut();
     modalLi.eq(indexChdek).find('button').focus();
     bigNarr.removeClass('action');
-  });
+    gallery.remove();
+  }); // galleryClose.on('click')
+//  -------------------------------
+});// mLiBtn.on('click')
 
 })(jQuery);
