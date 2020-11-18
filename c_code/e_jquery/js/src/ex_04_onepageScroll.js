3ab270019f7ab699f6bf43f26e6b51b2c0c2dc30
xido <xidoweb@gmail.com>
2020-11-18 18:04:06 +0900

    onepage and tabmenmu

--- c_code/e_jquery/js/src/ex_04_onepageScroll.js
+++ c_code/e_jquery/js/src/ex_04_onepageScroll.js
@@ -95,6 +95,24 @@ tabLi.on('click',['a'], function(e){
   tabCon.eq(itI).show();
   tabCon.eq(itI).siblings().hide();
 });
+// --------------------------------------------------
+var titleTab = $('.title_tab_style');
+var titleTabDl = titleTab.children('dl');
+var titleTabDt = titleTabDl.children('dt');
+var titleTabDd = titleTabDl.children('dd');
 
+titleTabDt.eq(0).addClass('action');
+
+titleTabDt.on('click', ['a'],function(e){
+  e.preventDefault();
+  var it = $(this);
+  
+  it.siblings('dt').removeClass('action');
+  it.addClass('action');
+
+  titleTabDd.hide();
+  it.next('dd').show();
+});
+// --------------------------------------------------
   // jQuery end
 })(jQuery);
