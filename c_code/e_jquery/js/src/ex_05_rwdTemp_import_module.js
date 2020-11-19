34b747848c1a079575348f0c5960b1cfbffe7c72
xido <xidoweb@gmail.com>
2020-11-19 16:40:43 +0900

    jquery responsiveweb import처리방법

--- c_code/e_jquery/js/src/ex_05_rwdTemp_import_module.js
+++ c_code/e_jquery/js/src/ex_05_rwdTemp_import_module.js
@@ -2,7 +2,30 @@
 
 (function($){
   var nowW = deviceSize();
-  console.log(nowW);
- 
+  // console.log(nowW);
+  var deviceType = ['mobile', 'table', 'laptop', 'pc', 'pcfull'];
+// ---------------------------------------------------------------------
+  var url = './rwdTemp/';
+  var headBox = $('#headBox');
+  var viewBox = $('#viewBox');
+
+//----------------------------------------------------------------------
+// viewBox import file
+var viewImport = function(){
+  viewBox.append('<script src="../js/src/temp_mobile.js"></script>');
+};
+//-------------------------------
+  if(nowW === deviceType[0]){
+
+    headBox.load( url + 'redTemp_headBox_mobile.html');
+    viewBox.load( url+ 'rwdTemp_mobile.html' , viewImport);
+
+  }else if(nowW === deviceType[1]){
+    headBox.load( url + 'redTemp_headBox_tablet.html' );
+    viewBox.load( url+ 'rwdTemp_tablet.html' , viewImport);
+  }else{
+    headBox.load( url + 'redTemp_headBox_pc.html' );
+    viewBox.load( url+ 'rwdTemp_pc.html' , viewImport);
+  }
 
 })(jQuery); 
