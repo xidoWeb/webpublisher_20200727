6d1dd63334ced4f7cfbd9ba15e7381ae851e2ff9
xido <xidoweb@gmail.com>
2020-11-23 17:38:25 +0900

    mousewheel 기능

--- c_code/e_jquery/js/src/ex_07_mousewheel.html.js
+++ c_code/e_jquery/js/src/ex_07_mousewheel.html.js
@@ -1,11 +1,28 @@
 // ex_07_mousewheel.html.js
 
 (function($){
+
+  // var win = $(window);
+  var web = $('html, body');
   var wrap = $('#wrap');
   var permission = true;
+  var count = 0;
+  var pageOffset = [];
+  var pageRoll = $('.page_roll');
+  var i=0;
+  for(; i<pageRoll.length; i+=1){
+    pageOffset[i] = pageRoll.eq(i).offset().top;
+  }
+  // console.log(pageOffset);
+
+
+  web.animate({scrollTop:pageOffset[0]});
   
   wrap.on('mousewheel DOMMouseScroll', function(e){
-
+    e.preventDefault();
+    if(permission){
+      permission = false; 
+ 
     // 마우스 휠 움직임 파악 
     // - 파이어폭스외 다른 브라우저
     // console.log( e.originalEvent.wheelDelta );
@@ -26,10 +43,25 @@
       mWheel = eOriginal.wheelDelta; 
     }
 
-    console.log(mWheel);
+    // console.log(mWheel);
 
+    if(mWheel < 0 && count < pageOffset.length-1 ){ //휠을 아래로 스크롤시
+      count += 1;
+    }else if(mWheel > 0 && count > 0){// 휠을 위로 스크롤시
+      count -= 1;
+    }
+    // console.log( count , ':' , pageOffset[count]);
+    web.animate({scrollTop:pageOffset[count] + 'px'}, function(){
+      setTimeout(function(){
+        permission = true;
+      }, 100);
+    });
+
+// -----------------------------------------------------------
     // 터치마우스 또는 노트북에있는 터치패드, 트랙패드 ... 
-    // 경우는 수치가 일정하지 않음
+    // 경우는 수치가 일정하지 않음 -> if문을 이용하여 처리
+
+    } // if(permission)
   });
 
 })(jQuery);
