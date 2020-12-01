166b59e74fb477893186e054d0a16d1ec11f37e1
xido <xidoweb@gmail.com>
2020-12-01 09:31:12 +0900

    more btn result

--- c_code/e_jquery/js/src/eq_08_moreBtn.js
+++ c_code/e_jquery/js/src/eq_08_moreBtn.js
@@ -9,32 +9,49 @@
     var people = data;
     var i=0;
     var myLen = i;
-    var myViewLen = 4;
+    var myViewLen = 10;
     var maxLen = people.length;
     var part = $('.part');
     var partUl = part.find('ul');
-    var insertCode = '<li><div class="image"></div><dl><dt>title</dt><dd>email</dd></dl></li>';
+    var insertCode = '<li><div class="num"></div><div class="image"></div><dl><dt>title</dt><dd>email</dd></dl></li>';
     var more = $('.more_btn');
     var moreBtn = more.children('button');
+    var liEq;
+// ---------------------------------------------------
+    var moreFn = function(j){
+      var k;
+      // if( j === undefined){ 
+      //   k = myViewLen; 
+      // }else{
+      //   k = j;
+      // }
 
-    myLen =  i + myViewLen;
-    for(; i < myLen; i+=1){
-      partUl.append(insertCode);      
-    }
+      // (j === undefined)? k = myViewLen : k = j;
 
-    moreBtn.on('click', function(e){
-      e.preventDefault();
-      myLen =  i + myViewLen;
+      k = j || myViewLen;
+
+      myLen =  i + k;
       for(; i < myLen; i+=1){
         if( i >= maxLen){
           more.remove();
           break
         }else {
           partUl.append(insertCode);  
+          liEq = partUl.children('li').eq(i);
+          liEq.find('.num').text( data[i].id );
+          liEq.find('.image').css({backgroundImage: 'url(' + data[i].image_data + ')' });
+          liEq.find('dt').text(data[i].first_name);
+          liEq.find('dd').text(data[i].address);
         }
       }// for
+    };
+    moreFn();
+// ---------------------------------------------------
+    moreBtn.on('click', function(e){
+      e.preventDefault();
+      moreFn(15);
     });
-    
+// ---------------------------------------------------
 
   });
 })(jQuery);
