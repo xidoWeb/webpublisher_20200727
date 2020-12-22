b751d22b903c19d3c761ba1ee28ca02ae71b96ce
xido <xidoweb@gmail.com>
2020-12-22 12:13:21 +0900

    parallax scale Set

---
+++ c_code/e_jquery/js/src/ex_15_parallax2.js
@@ -0,0 +1,36 @@
+// ex_15_parallax2.js
+
+(function($){
+  var win = $(window);
+  var winH = win.outerHeight();
+
+  var headBox = $('#headBox');
+  var frontImg = headBox.find('.front_image');
+  var backImg = headBox.find('.back_image');
+
+  var setN = 1.5;
+
+  // 브라우저 스크롤시 수행
+  // 1. frontImg가 점점 커지게( transform:scale() );
+  // 2. 점점 사라지게
+  win.on('scroll', function(){
+    var winSt = $(this).scrollTop();
+    var per = winSt / winH;
+    var scaleR = 1+per;
+    var opacityR;
+    
+    if(scaleR <= setN){
+      console.log('scale: ', 1 + per);
+      frontImg.css({transform:'scale(' + scaleR + ')'});
+    }else if(scaleR >= setN + 1){      
+      console.log('opacity: ', opacityR+1);
+      opacityR = setN + 1 - per;    
+      frontImg.css({opacity: opacityR});
+    }
+
+
+  }); // win.on('scroll')
+
+
+
+})(jQuery);
