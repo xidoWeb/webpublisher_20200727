7d18954a3211e28e5b8744b9baae440a3a2a0b6d
xido <xidoweb@gmail.com>
2020-12-22 13:12:48 +0900

    parallax2

--- c_code/e_jquery/js/src/ex_15_parallax2.js
+++ c_code/e_jquery/js/src/ex_15_parallax2.js
@@ -20,23 +20,38 @@
     var opacityR, airPer, logoPer;
 
   // 4. logo 사라지게 만들기 
+  // 5. logo사라지는 동안 앞에 투명막 생기게하기
   var logoR = 0.5;
    if(per >= logoR){
     logoPer = (1 + logoR) - per;
      console.log( logoR + per );    
     h1.css({opacity: logoPer, transform:'scale('+ (logoR + per) + ')' });
+
+    headBox.addClass('default');
+
+   }else{
+     headBox.removeClass('default');
    }
 
   // 1. frontImg가 점점 커지게( transform:scale() );
   // 2. 점점 사라지게
+  opacityR = setN + 1 - per; 
+
     if(scaleR <= setN){
       // console.log('scale: ', 1 + per);
       frontImg.css({transform:'scale(' + scaleR + ')'});
     }else if(scaleR >= setN + 1){      
-      // console.log('opacity: ', opacityR+1);
-      opacityR = setN + 1 - per;    
+      // console.log('opacity: ', opacityR+1);         
       frontImg.css({opacity: opacityR});
     }
+
+  // 6. frontImg 의 투명도가 0이되면 headBox를 사라지게 만들기
+  if(opacityR < 0){
+    // console.log('사라진위치: ', win.scrollTop() );
+    headBox.hide();
+  }else{
+    headBox.show();
+  }
 
   // 3. 비행기 나타나면서 올라가기
     if(scaleR > setN){
