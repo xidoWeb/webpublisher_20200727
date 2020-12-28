e5f04b28841fe28dd42c349f49b30b30392db94d
xido <xidoweb@gmail.com>
2020-12-28 16:04:30 +0900

    random Color

--- c_code/e_jquery/js/src/ex_18_like_pinterest2.js
+++ c_code/e_jquery/js/src/ex_18_like_pinterest2.js
@@ -3,7 +3,23 @@
 (function($){
   // 1. 카드 감싸는영역 크기설정, 내용물 삽입 
   // 2. 스크롤의 진행위치값 표시
-  //
+  // 3. 스크롤시 일정위치가 넘어가면 추가 카드가 생성되게 처리
+  // 4. Math.random()을 이용하여 색상, 높이값을 변경
+
+  var r1 = 
+    Math.floor(Math.random() * 16).toString(16);
+  var r2 = 
+    Math.floor(Math.random() * 16).toString(16);
+  var r3 = 
+    Math.floor(Math.random() * 16).toString(16);
+var Rn = function(){
+  return  Math.floor(Math.random()*16).toString(16);
+};
+  var randomColor = '#'+Rn()+Rn()+Rn();  
+  console.log(randomColor);
+
+// ================================
+// 2번기능 스크롤시 진행위치값
   var headBox = $('#headBox');
   headBox.prepend('<div class="scroll_percent"></div>');
   var scrollP = $('.scroll_percent ');
@@ -13,8 +29,8 @@
     backgroundColor:'#f06',
     transform:'translate(-0.5rem) scale(1.1) skew(-15deg)'
   });
-  
-  // ----------------------------------
+// 2번기능 세팅 ----------------------------------
+// ================================
   var cardCode = '<div class="card"><div class="card_img"></div><div class="card_narr"><a href="#"><dl><dt>title</dt><dd>Lorem, ipsum.</dd></dl></a></div></div>';
 
   // window, cardBox, card_area 선택자
@@ -24,15 +40,24 @@
 
   // 추가 선택자
   var card; 
-  // card 생성
-  var i=0; 
-  var cardCount = 50;
+  // 1. card 생성
+  // 3번에서 요구하는 카드생성형태이므로 중복 수행이기에 함수화 처리
+  var i=0;   
+  var cardCount = 0;
 
-  for(; i < cardCount; i++){
-    cardArea.append(cardCode);
-    card = cardArea.children('.card').eq(i);
-    card.find('dt').text( 'title_'+ (i+1) );
-  }
+  var AppendCardFn = function(){
+    cardCount += 50; 
+
+    for(; i < cardCount; i++){
+      cardArea.append(cardCode);
+      card = cardArea.children('.card').eq(i);
+      card.find('dt').text( 'title_'+ (i+1) );
+    }
+
+    $('.card').css({float:'left'});
+
+  }// AppendCardFn();
+  AppendCardFn();
 
   // .card_area의 가로값 세팅
   var cardEq0 = $('.card').eq(0);
@@ -43,15 +68,17 @@
     cardWidthLen = 
     parseInt(cardBox.outerWidth() / cardWidth);
     cardArea.css({width:cardWidth * cardWidthLen + 'px'});
+    
   }// CardWidthSet();
   CardWidthSet();
 
-  $('.card').css({float:'left'});
   // 브라우저 크기 변경시 cardArea 사이즈 재수정
   win.on('resize', function(){
     CardWidthSet();
   });
 
+// -----------------------------------
+// 2번기능 스크롤시처리 
   win.on('scroll', function(){
     var st = $(this).scrollTop();
     var wrap = $('#wrap');
@@ -64,8 +91,14 @@
     // console.log(per);
     // .scroll_percent 의 가로값에 적용
     scrollP.css({width: per + '%'});
-  });
 
+    // 3번기능 일정수치가 넘어가면 추가 카드를 생성
+    // 카드를 생성한 영역을 재호출, 기존의 카드는 유지
+    if(per >= 100){
+      AppendCardFn();
+    }
+  });
+// -----------------------------------
 
 
 
